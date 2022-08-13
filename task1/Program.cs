7b3e05d5ee2c42e6f4aa29f104a294abbd96405d
Rishat <risatnurullin53@gmail.com>
2022-08-13 10:33:09 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+Console.Write("Введите числа через пробел ");
+
+int[] array = Console.ReadLine(). Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] >= 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("В массиве " + '[' + string.Join(", ", arr) + ']' + " количество положительных чисел = " + count);
